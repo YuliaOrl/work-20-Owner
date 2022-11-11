0a2c2191f2ae41efb97511b83012f1ffb8253219
YuliaOrl <orbitalik32@gmail.com>
2022-11-11 20:58:07 +0300

    Added WebShop refactoring tests

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/${env}.properties",
+        "classpath:${env}.properties",
         "file:~/${env}.properties",
         "file:./${env}.properties"
 })
